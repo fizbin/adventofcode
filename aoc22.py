2be0c38f75683202a9a32239c85da534ac9f514a
Daniel Martin <daniel.martin@crowdstrike.com>
2021-12-22 01:28:58 -0500

    Day 22 in python, cleaned up

--- aoc22.py
+++ aoc22.py
@@ -8,39 +8,54 @@ import numpy as np
 
 if __name__ == "__main__":
     with open("aoc22.in" if len(sys.argv) < 2 else sys.argv[1], encoding="utf-8") as f:
-        data = re.findall(r'(on|off) x=([-\d]+)\.\.([-\d]+),y=([-\d]+)\.\.([-\d]+),z=([-\d]+)\.\.([-\d]+)', f.read())
-        fmtdata = [(t[0] == 'on', (int(t[1]),int(t[2])),
-                    (int(t[3]),int(t[4])),
-                    (int(t[5]),int(t[6]))
-                   ) for t in data]
-    print(len(fmtdata))
-    print((fmtdata[0]))
-    stuff = np.zeros((1000,1000,1000), dtype=np.int8)
+        data = re.findall(
+            r"(on|off) x=([-\d]+)\.\.([-\d]+),y=([-\d]+)\.\.([-\d]+),z=([-\d]+)\.\.([-\d]+)",
+            f.read(),
+        )
+        fmtdata = [
+            (
+                t[0] == "on",
+                (int(t[1]), int(t[2])),
+                (int(t[3]), int(t[4])),
+                (int(t[5]), int(t[6])),
+            )
+            for t in data
+        ]
+    stuff = np.zeros((1000, 1000, 1000), dtype=np.int8)
     for blk in fmtdata:
         if -60 < blk[1][0] < 60:
-            stuff[blk[1][0]+50:blk[1][1]+51,blk[2][0]+50:blk[2][1]+51,blk[3][0]+50:blk[3][1]+51] = int(blk[0])
+            stuff[
+                blk[1][0] + 50 : blk[1][1] + 51,
+                blk[2][0] + 50 : blk[2][1] + 51,
+                blk[3][0] + 50 : blk[3][1] + 51,
+            ] = int(blk[0])
     print(stuff.sum())
 
-    all_x = sorted(sum([[blk[1][0],blk[1][1]+1] for blk in fmtdata], []))
-    all_y = sorted(sum([[blk[2][0],blk[2][1]+1] for blk in fmtdata], []))    
-    all_z = sorted(sum([[blk[3][0],blk[3][1]+1] for blk in fmtdata], []))
-    stuff = np.zeros((len(all_x),len(all_y),len(all_z)), dtype=np.int64)
+    all_x = sorted(sum([[blk[1][0], blk[1][1] + 1] for blk in fmtdata], []))
+    all_y = sorted(sum([[blk[2][0], blk[2][1] + 1] for blk in fmtdata], []))
+    all_z = sorted(sum([[blk[3][0], blk[3][1] + 1] for blk in fmtdata], []))
+    stuff = np.zeros((len(all_x), len(all_y), len(all_z)), dtype=np.int8)
     for blk in fmtdata:
         ix1 = all_x.index(blk[1][0])
-        ix2 = all_x.index(blk[1][1]+1)
+        ix2 = all_x.index(blk[1][1] + 1)
         iy1 = all_y.index(blk[2][0])
-        iy2 = all_y.index(blk[2][1]+1)
+        iy2 = all_y.index(blk[2][1] + 1)
         iz1 = all_z.index(blk[3][0])
-        iz2 = all_z.index(blk[3][1]+1)
-        stuff[ix1:ix2,iy1:iy2,iz1:iz2] = int(blk[0])
-    print(stuff.sum())
-    print(stuff.shape)
+        iz2 = all_z.index(blk[3][1] + 1)
+        stuff[ix1:ix2, iy1:iy2, iz1:iz2] = int(blk[0])
+    # print(stuff.sum())
     def szfunc(x, ax):
-        xdim = (ax + [ax[-1] + 1])[x:x+2]
-        return (xdim[1] - xdim[0])
-    xfoo = np.array([szfunc(x, all_x) for x in range(len(all_x))]).reshape((len(all_x), 1, 1))
-    yfoo = np.array([szfunc(y, all_y) for y in range(len(all_y))]).reshape((1, len(all_y), 1))
-    zfoo = np.array([szfunc(z, all_z) for z in range(len(all_z))]).reshape((1,1,len(all_z)))
+        xdim = (ax + [ax[-1] + 1])[x : x + 2]
+        return xdim[1] - xdim[0]
+
+    xfoo = np.array(
+        [szfunc(x, all_x) for x in range(len(all_x))], dtype=np.uint64
+    ).reshape((len(all_x), 1, 1))
+    yfoo = np.array(
+        [szfunc(y, all_y) for y in range(len(all_y))], dtype=np.uint64
+    ).reshape((1, len(all_y), 1))
+    zfoo = np.array(
+        [szfunc(z, all_z) for z in range(len(all_z))], dtype=np.uint64
+    ).reshape((1, 1, len(all_z)))
     sz = xfoo * yfoo * zfoo
-    print(sz.shape)
-    print((stuff * sz).sum())
+    print(np.where(stuff, sz, np.uint64(0)).sum())
