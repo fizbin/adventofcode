3933ef18ef4b540773da7cc9474fb3e9961f398a
Daniel Martin <fizbin@gmail.com>
2022-12-04 08:21:41 -0500

    Pretty up day 4 haskell

--- aoc4.hs
+++ aoc4.hs
@@ -1,19 +1,19 @@
+import Data.Char (isAsciiLower, isAsciiUpper, ord)
+import Data.Set (findMin, fromList, intersection)
 import System.Environment
-import Data.Set (fromList, intersection, findMin)
-import Data.Char (ord, isAsciiUpper, isAsciiLower)
 
 parseLine :: String -> (Int, Int, Int, Int)
 parseLine s0 = head $ parses1 s0
   where
     parses1 s = [(x, y, z, w) | (x, s') <- reads s, (y, z, w) <- parses2 s']
-    parses2 s = [(y, z, w) | ('-':s') <- [s], (y, s'') <- reads s', (z, w) <- parses3 s'']
-    parses3 s = [(z, w) | (',':s') <- [s], (z, s'') <- reads s', ('-':s''') <- [s''], (w, _) <- reads s''']
+    parses2 s = [(y, z, w) | ('-' : s') <- [s], (y, s'') <- reads s', (z, w) <- parses3 s'']
+    parses3 s = [(z, w) | (',' : s') <- [s], (z, s'') <- reads s', ('-' : s''') <- [s''], (w, _) <- reads s''']
 
-bad1 :: (Int,Int,Int,Int) -> Bool
-bad1 (a,b,c,d) = ((a <= c) && (d <= b)) || ((c <= a) && (b <= d))
+bad1 :: (Int, Int, Int, Int) -> Bool
+bad1 (a, b, c, d) = ((a <= c) && (d <= b)) || ((c <= a) && (b <= d))
 
-bad2 :: (Int,Int,Int,Int) -> Bool
-bad2 (a,b,c,d) = not $ (b < c) || (d < a)
+bad2 :: (Int, Int, Int, Int) -> Bool
+bad2 (a, b, c, d) = not $ (b < c) || (d < a)
 
 main :: IO ()
 main = do
