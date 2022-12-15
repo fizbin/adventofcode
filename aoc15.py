b581da738e5721aa61beded5eb91dec21510b790
Daniel Martin <fizbin@gmail.com>
2022-12-15 00:55:44 -0500

    day 15, python

--- aoc15.py
+++ aoc15.py
@@ -1,5 +1,4 @@
-from aoc_util import *
-import numpy as np
+from aoc_util import numbers, get_data_lines
 
 data = get_data_lines(15)
 
@@ -7,22 +6,24 @@ sensors = []
 for line in data:
     sensors.append(tuple(numbers(line)))
 
-targety = 2000000
-excluded = []
-beacons = set()
-for sensor in sensors:
-    mnhatd = abs(sensor[0]-sensor[2]) + abs(sensor[1] - sensor[3])
-    d_to_target = abs(sensor[1] - targety)
-    span = mnhatd - d_to_target
-    if span >= 0:
-        range_x = (sensor[0] - span, sensor[0] + span)
-        excluded.append(range_x)
-    if sensor[3] == targety:
-        beacons.add(sensor[2])
+def find_excluded(targety):
+    excluded = []
+    beacons = set()
+    for sensor in sensors:
+        mnhatd = abs(sensor[0]-sensor[2]) + abs(sensor[1] - sensor[3])
+        d_to_target = abs(sensor[1] - targety)
+        span = mnhatd - d_to_target
+        if span >= 0:
+            range_x = (sensor[0] - span, sensor[0] + span)
+            excluded.append(range_x)
+        if sensor[3] == targety:
+            beacons.add(sensor[2])
 
-excluded.sort()
+    excluded.sort()
+    return (excluded, beacons)
+
+(excluded, beacons) = find_excluded(2000000)
 tot = 0
-print(excluded)
 currx = excluded[0][0] - 6
 for (xmin, xmax) in excluded:
     xmin = max(currx+1, xmin)
@@ -32,3 +33,26 @@ for (xmin, xmax) in excluded:
     tot += (xmax - xmin + 1)
     tot -= len(list(b for b in beacons if xmin <= b <= xmax))
 print(tot)
+
+for y in range(4000000):
+    (excluded, _) = find_excluded(y)
+    currx = -1
+    tot = 0
+    used_ex = []
+    for (xmin, xmax) in excluded:
+        xmin = max(currx+1, xmin)
+        xmax = min(xmax, 4000000)
+        if xmin > xmax:
+            continue
+        currx = xmax
+        tot += (xmax - xmin + 1)
+        used_ex.append((xmin,xmax))
+        if xmax == 4000000:
+            break
+    if tot < 4000001:
+        # print (y, tot, used_ex)
+        for ((a, b),(c,d)) in zip(used_ex, used_ex[1:]):
+            if b < c - 1:
+                x = c-1
+        print(y + 4000000*x)
+        break
