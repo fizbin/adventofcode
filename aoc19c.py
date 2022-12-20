788e0e7c5baf6826778f1fb5a4f9010e5cc7e5c7
Daniel Martin <fizbin@gmail.com>
2022-12-19 20:05:24 -0500

    Day 19, alt python slow, but working

--- aoc19c.py
+++ aoc19c.py
@@ -6,12 +6,12 @@ import re
 import heapq
 
 # under the situation of infinite ore, what's the max geode?
-def pot_geo_total(time, robos, rocks, blueprint, maxt):
+def geo_overstimate(timeleft, robos, rocks, blueprint):
     (_a, crocks, orocks, grocks) = rocks
     (_a, crobos, orobos, grobos) = robos
     # if time == 8 and sum(robos) == 1:
     #     breakpoint()
-    for tick in range(time, maxt):
+    for _ in range(timeleft):
         grocks += grobos
         orocks += orobos
         crocks += crobos
@@ -24,21 +24,45 @@ def pot_geo_total(time, robos, rocks, blueprint, maxt):
         crobos += 1
     return grocks
 
+def geo_underestimate(timeleft, robos, rocks, blueprint):
+    return timeleft * robos[3] + rocks[3]
+
 def prune_state(tick, state, blueprint, maxt):
     max_do_nothing = max((maxt - tick)*st[0][3] + st[1][3] for st in state)
-    return set(st for st in state if pot_geo_total(tick, st[0], st[1], blueprint, maxt) >= max_do_nothing)
+    return set(st for st in state if geo_overstimate(maxt - tick, st[0], st[1], blueprint) >= max_do_nothing)
+
+def rocks_le(rocks1, rocks2):
+    return all(rocks1[i] <= rocks2[i] for i in range(4))
 
 # ore, clay, obsidian, geode
-def do_minute(blueprint, tick, state):
+def do_minute(blueprint, timeleft, state, fired_opt_tracker):
+    robomaxb = [0,0,0,0]
+    for oretype in range(4):
+        orereq = max(blueprint[i][oretype] for i in range(4))
+        robomaxb[oretype] = orereq
+    robomaxb[3] = 999
+    robomax = tuple(robomaxb)
     retval1 = set()
+    seenrocks = {}
+    min_geo_overestimate = 1
     for (robos, rocks) in state:
         # factory does nothing
         nextrocks = (rocks[0] + robos[0], rocks[1] + robos[1], rocks[2] + robos[2], rocks[3] + robos[3])
+        min_geo_overestimate = max(min_geo_overestimate, geo_underestimate(timeleft, robos, rocks, blueprint))
+        if robos in seenrocks:
+            if not rocks_le(rocks, seenrocks[robos]):
+                seenrocks[robos] = rocks
+        else:
+            seenrocks[robos] = rocks
         retval1.add((robos, nextrocks))
+    for (robos, rocks) in state:
+        nextrocks = (rocks[0] + robos[0], rocks[1] + robos[1], rocks[2] + robos[2], rocks[3] + robos[3])
         # factory does something
         for fac_action in range(4):
 #            print(repr(blueprint))
             if any(blueprint[fac_action][i] > rocks[i] for i in range(4)):
+                continue
+            if robos[fac_action] == robomax[fac_action]:
                 continue
             lrobos = list(robos)
             lnrocks = list(nextrocks)
@@ -47,7 +71,13 @@ def do_minute(blueprint, tick, state):
                 lnrocks[i] -= blueprint[fac_action][i]
             if any(r < 0 for r in lnrocks):
                 breakpoint()
-            retval1.add((tuple(lrobos), tuple(lnrocks)))
+            if geo_overstimate(timeleft, lrobos, lnrocks, blueprint) >= min_geo_overestimate:
+                lrobost = tuple(lrobos)
+                if lrobost in seenrocks and rocks_le(lnrocks, seenrocks[lrobost]):
+                    fired_opt_tracker[0] += 1
+                    continue
+                retval1.add((lrobost, tuple(lnrocks)))
+                min_geo_overestimate = max(min_geo_overestimate, geo_underestimate(timeleft, lrobos, lnrocks, blueprint))
     return retval1
 
 # ore, clay, obsidian, geode
@@ -75,14 +105,31 @@ blueprints = [parse_blueprint(line) for line in data]
 results = {}
 for blueprint in blueprints:
     state = set([((1,0,0,0),(0,0,0,0))])
+    max_state_sz = 1
+    fired_opt_tracker = [0]
     for tick in range(24):
-        prune_state(tick, state, blueprint[1], 24)
-        state = do_minute(blueprint[1], tick, state)
-        print(blueprint[0], tick, len(state)) # , sorted(state, key=lambda s:s[1][3])[-1])
+        state = do_minute(blueprint[1], 23 - tick, state, fired_opt_tracker)
+        max_state_sz = max(max_state_sz, len(state))
+        # print(blueprint[0], tick, len(state)) # , sorted(state, key=lambda s:s[1][3])[-1])
     results[blueprint[0]] = max(poss[1][3] for poss in state)
+    print(f"For {blueprint} got {results[blueprint[0]]} with at most {max_state_sz} states, rocks_le opt fired {fired_opt_tracker[0]} times")
 
 tot = 0
 for (n, geo) in results.items():
-    print(n,geo)
+    # print(n,geo)
     tot += n * geo
+print(tot)
+
+results = {}
+for blueprint in blueprints[0:3]:
+    state = set([((1,0,0,0),(0,0,0,0))])
+    for tick in range(32):
+        state = do_minute(blueprint[1], 31 - tick, state, [0])
+        print(blueprint[0], tick, len(state)) # , sorted(state, key=lambda s:s[1][3])[-1])
+    results[blueprint[0]] = max(poss[1][3] for poss in state)
+    print(f"For {blueprint} got {results[blueprint[0]]}")
+
+tot = 1
+for (n, geo) in results.items():
+    tot *= geo
 print(tot)
