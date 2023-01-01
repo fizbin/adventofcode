fc1bf61fdee448ba45f33c83c3bf919e2c4c7ec8
Daniel Martin <fizbin@gmail.com>
2023-01-01 18:01:25 -0500

    Day 23 haskell speed improvements

--- aoc23.hs
+++ aoc23.hs
@@ -1,12 +1,13 @@
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TupleSections #-}
 {-# OPTIONS_GHC -Wall #-}
 
-import qualified Data.Map as M
-import Data.Maybe (mapMaybe, listToMaybe, fromMaybe)
-import qualified Data.Set as S
 import Data.List
+import qualified Data.Map as M
+import Data.Maybe (fromMaybe, listToMaybe, mapMaybe)
+import qualified Data.Set as S
 import System.Environment (getArgs)
+import qualified Data.Array.IArray as IA
+import Data.Array.Unboxed (UArray)
 
 -- import Debug.Trace (trace)
 
@@ -18,26 +19,41 @@ infixl 6 $+$
 rotRight :: Num b => (b, a) -> (a, b)
 rotRight (a, b) = (b, -a)
 
+southNbs :: [(Int, Int)]
+westNbs :: [(Int, Int)]
+northNbs :: [(Int, Int)]
+eastNbs :: [(Int, Int)]
+southNbs = [(1, -1), (1, 0), (1, 1)]
+westNbs = map rotRight southNbs
+northNbs = map rotRight westNbs
+eastNbs = map rotRight northNbs
+
+allNeighbors :: (Int, Int) -> [(Int, Int)]
+allNeighbors (a, b) = [(a + i, b + j) | i <- [-1, 0, 1], j <- [-1, 0, 1], (i /= 0) || (j /= 0)]
+
 doStep :: Int -> S.Set (Int, Int) -> Maybe (S.Set (Int, Int))
 doStep tick elves =
-    let nxt = S.map (\e -> M.findWithDefault e e proposedMap) elves
+    let nxt = foldr S.delete crowded proposedMapRev `S.union` lonely `S.union` M.keysSet proposedMapRev
      in if nxt == elves then Nothing else Just nxt
   where
-    allNeighbors (a, b) = [(a + i, b + j) | i <- [-1, 0, 1], j <- [-1, 0, 1], (i /= 0) || (j /= 0)]
-    southNbs = [(1, -1), (1, 0), (1, 1)]
-    westNbs = map rotRight southNbs
-    northNbs = map rotRight westNbs
-    eastNbs = map rotRight northNbs
+    arange =
+        foldr
+            ( \(elfx, elfy) ((minx, miny), (maxx, maxy)) ->
+                ((min minx (elfx - 1), min miny (elfy - 1)), (max maxx (elfx + 1), max maxy (elfy + 1)))
+            )
+            ((0, 0), (0, 0))
+            elves
+    elfArray = IA.accumArray (const id) True arange [(e, False) | e <- S.toList elves] :: UArray (Int, Int) Bool
+    blankspot coord = elfArray IA.! coord
     potdirs = take 4 $ drop (tick `mod` 4) [northNbs, southNbs, westNbs, eastNbs, northNbs, southNbs, westNbs]
-    proposed elf = let allProposed = map (map (elf $+$)) potdirs
-                       filteredP = filter (all (`S.notMember` elves)) allProposed
-                    in if all (`S.notMember` elves) (allNeighbors elf) then Nothing
-                    else head . tail <$> listToMaybe filteredP
-    proposedMapSrc = mapMaybe (\e -> (e,) <$> proposed e) $ S.toList elves
-    one = 1 :: Int
-    valueTimes = M.fromListWith (+) $ map (\(_, b) -> (b, one)) proposedMapSrc
-    proposedMap = M.fromList $ filter (\(_, e) -> valueTimes M.! e < 2) proposedMapSrc
-
+    (lonely, crowded) = S.partition (all blankspot . allNeighbors) elves
+    proposed elf =
+        let allProposed = map (map (elf $+$)) potdirs
+            filteredP = filter (all blankspot) allProposed
+         in head . tail <$> listToMaybe filteredP
+    proposedMapSrc = mapMaybe (\e -> (, Just e) <$> proposed e) $ S.toList crowded
+    -- dest -> src
+    proposedMapRev = M.mapMaybe id $ M.fromListWith (const $ const Nothing) proposedMapSrc
 
 main :: IO ()
 main = do
@@ -50,11 +66,11 @@ main = do
     let elves = S.fromList $ [(r, c) | (r, line) <- zip [0 ..] s, (c, ch) <- zip [0 ..] line, ch == '#']
     -- part 1
     -- let applyN :: Int -> (a->a) -> a -> a; applyN 0 _ x = x; applyN n f x = applyN (n-1) f (f x)
-    let tenSteps = foldl' (\e t -> fromMaybe e (doStep t e)) elves [0..9]
+    let tenSteps = foldl' (\e t -> fromMaybe e (doStep t e)) elves [0 .. 9]
     let spanWidth vs = maximum vs - minimum vs + 1
     let area = spanWidth (S.map fst tenSteps) * spanWidth (S.map snd tenSteps)
     print $ area - S.size tenSteps
 
     -- part 2
-    let p2ans n e = maybe (n+1) (p2ans $ n+1) (doStep n e)
+    let p2ans n e = maybe (n + 1) (p2ans $ n + 1) (doStep n e)
     print $ p2ans 0 elves
