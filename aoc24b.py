e50e1ddc3193fb28a21ad5f5a3812ac51e2e77f3
Daniel Martin <fizbin@gmail.com>
2023-01-01 21:32:36 -0500

    Add numpy-based day 24 solution

--- aoc24b.py
+++ aoc24b.py
@@ -4,27 +4,28 @@ from scipy.ndimage import convolve
 
 data = chargrid(get_data(24))
 
-pitch = np.array(data, dtype=np.str_)[1:-1,1:-1]
-s_blizzards = (pitch != 'v').astype(int)
-n_blizzards = (pitch != '^').astype(int)
-e_blizzards = (pitch != '>').astype(int)
-w_blizzards = (pitch != '<').astype(int)
-kernel = np.array([[0,1,0],[1,1,1],[0,1,0]], dtype=int)
+pitch = np.array(data, dtype=np.str_)[1:-1, 1:-1]
+s_blizzards = (pitch != "v").astype(int)
+n_blizzards = (pitch != "^").astype(int)
+e_blizzards = (pitch != ">").astype(int)
+w_blizzards = (pitch != "<").astype(int)
+kernel = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], dtype=int)
+
 
 def find_exit(initial, goal, initial_time: int):
     working = np.zeros(pitch.shape, dtype=int)
-    t = initial_time+1
+    t = initial_time + 1
     while True:
-        working = convolve(working, kernel, mode='constant', cval=0)
+        working = convolve(working, kernel, mode="constant", cval=0).astype(bool).astype(int)
         working[initial] = 1
         working *= np.roll(s_blizzards, t, axis=0)
         working *= np.roll(n_blizzards, -t, axis=0)
         working *= np.roll(e_blizzards, t, axis=1)
         working *= np.roll(w_blizzards, -t, axis=1)
-        working[working.nonzero()] = 1
         if working[goal]:
-            return t+1
-        t = t+1
+            return t + 1
+        t = t + 1
+
 
 pitchheight, pitchwidth = pitch.shape
 
