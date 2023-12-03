2c06c814fefb8ed176356295023077495314688b
Daniel Martin <fizbin@gmail.com>
2023-12-03 08:26:22 -0500

    2023 day 3, python and haskell

---
+++ aoc2023/aoc3.hs
@@ -0,0 +1,50 @@
+import Control.Monad (guard, join)
+import Data.Char (isDigit)
+import Data.List (tails)
+import Data.Map.Strict qualified as M
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    let filename =
+            if null args
+                then "aoc3.in"
+                else head args
+
+    -- need to replace '.' with something else because the reads instance for Int
+    -- will return [] when handed "5.4" but will return [(5, "x4")] when handed "5x4".
+    -- Can't use space instead of dot because skipping leading spaces would also be bad
+    s0 <- lines . map (\ch -> if ch == '.' then 'x' else ch) <$> readFile filename
+    let s = replicate (2 + length (head s0)) 'x' : ["x" ++ ln ++ "x" | ln <- s0] ++ [replicate (2 + length (head s0)) 'x']
+    let chMap = M.fromList $ do
+            (rowidx, row) <- zip [0 :: Int ..] s
+            (colidx, ch) <- zip [0 :: Int ..] row
+            pure ((rowidx, colidx), ch)
+    let goodnums = do
+            (rowidx, row) <- zip [0 :: Int ..] s
+            (pstart : start) <- tails row
+            let colidx = length row - length start
+            (num, rest) <- if not (isDigit pstart) then reads start else []
+            let numlen = length start - length rest
+            let above = take (2 + numlen) $ drop (colidx - 1) (s !! (rowidx - 1))
+            let below = take (2 + numlen) $ drop (colidx - 1) (s !! (rowidx + 1))
+            let neighbors = [pstart, head rest]
+            guard $ not (all isDigit (filter (/= 'x') $ above ++ below ++ neighbors))
+            pure (num + 0 :: Int)
+    print $ sum goodnums
+    let asternums = do
+            (rowidx, row) <- zip [0 :: Int ..] s
+            (pstart : start) <- tails row
+            let colidx = length row - length start
+            (num, rest) <- if not (isDigit pstart) then reads start else []
+            let numlen = length start - length rest
+            (asteri, asterj) <-
+                [(rowidx, colidx - 1), (rowidx, colidx + numlen)]
+                    ++ join
+                        [ [(rowidx - 1, col), (rowidx + 1, col)] | col <- [colidx - 1 .. colidx + numlen]
+                        ]
+            guard $ M.lookup (asteri, asterj) chMap == Just '*'
+            pure ((asteri, asterj), [num + 0 :: Int])
+    let asterMap = M.fromListWith (++) asternums
+    print $ sum $ map (\v -> head v * head (tail v)) $ filter ((== 2) . length) $ M.elems asterMap
---
+++ aoc2023/aoc3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from aoc_util import *
+import re
+
+data = get_data_lines(3)
+
+fulldata = ["." * len(data[0])] + data + ["." * len(data[0])]
+fulldata = ["." + x + "." for x in fulldata]
+gear_map = {}
+total = 0
+for row in range(1, len(fulldata)):
+    for num_match in re.finditer(r"\d+", fulldata[row]):
+        s, e = num_match.start(0), num_match.end(0)
+        num = int(num_match.group(0))
+        num2 = num
+        surrounding_spots = (
+            [(row, s - 1), (row, e)]
+            + [(row - 1, c) for c in range(s - 1, e + 1)]
+            + [(row + 1, c) for c in range(s - 1, e + 1)]
+        )
+        for i, j in surrounding_spots:
+            ch = fulldata[i][j : j + 1]
+            if re.match(r"[^\d.\s]", ch):
+                total += num
+                num = 0
+            if ch == "*":
+                gear_map.setdefault((i, j), []).append(num2)
+print(total)
+
+total_gear = 0
+for adjs in gear_map.values():
+    if len(adjs) == 2:
+        total_gear += adjs[0] * adjs[1]
+print(total_gear)
