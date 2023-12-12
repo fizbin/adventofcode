c6b2cafc2369d6aaf883db2b8b778a2f4ea031d5
Daniel Martin <fizbin@gmail.com>
2023-12-12 11:53:36 -0500

    2023 day 12, python reformatted

--- aoc2023/aoc12.py
+++ aoc2023/aoc12.py
@@ -5,51 +5,55 @@ import re
 import itertools
 from functools import cache
 
+
 def handle_line1(line: str):
-    (fixrecord, seqnumstr) = line.split(' ')
+    (fixrecord, seqnumstr) = line.split(" ")
     seqnums = [int(s) for s in seqnumstr.split(",")]
-    validateregex = re.compile('^.*' + '[.]+'.join("#" * s for s in seqnums) + '.*$')
-    numpounds = len([x for x in fixrecord if x=='#'])
-    questionspots = [idx for (idx, ch) in enumerate(fixrecord) if ch == '?']
+    validateregex = re.compile("^.*" + "[.]+".join("#" * s for s in seqnums) + "[.]*$")
+    numpounds = len([x for x in fixrecord if x == "#"])
+    questionspots = [idx for (idx, ch) in enumerate(fixrecord) if ch == "?"]
     totseqnums = sum(seqnums)
     neededpounds = totseqnums - numpounds
     retval = 0
-    alldots = fixrecord.replace('?', '.')
+    alldots = fixrecord.replace("?", ".")
     if neededpounds >= 0:
         for combo in itertools.combinations(questionspots, neededpounds):
             test = list(alldots)
             for i in combo:
-                test[i] = '#'
-            if validateregex.match(''.join(test)):
+                test[i] = "#"
+            if validateregex.match("".join(test)):
                 retval += 1
     # print("DBG", line, ":", retval)
     return retval
 
+
 def embiggen(line):
-    (fixrecord, seqnumstr) = line.split(' ')
+    (fixrecord, seqnumstr) = line.split(" ")
     return f"{'?'.join([fixrecord]*5)} {','.join([seqnumstr]*5)}"
 
+
 def handle_line2(line: str):
-    (fixrecord, seqnumstr) = line.split(' ')
+    (fixrecord, seqnumstr) = line.split(" ")
     seqnums = [int(s) for s in seqnumstr.split(",")]
 
     @cache
     def combos(str_rem, seq_rem):
         if not seq_rem:
-            if '#' in str_rem:
+            if "#" in str_rem:
                 return 0
             return 1
         if not str_rem:
             return 0
         retval = 0
-        if str_rem[0] in '.?':
+        if str_rem[0] in ".?":
             retval += combos(str_rem[1:], seq_rem)
-        if str_rem[0] in '?#':
-            if len(str_rem) < seq_rem[0] or '.' in str_rem[0:seq_rem[0]]:
+        if str_rem[0] in "?#":
+            if len(str_rem) < seq_rem[0] or "." in str_rem[0 : seq_rem[0]]:
                 retval += 0
             else:
-                if str_rem[seq_rem[0]:seq_rem[0]+1] in '?.':
-                    retval += combos(str_rem[seq_rem[0]+1:], seq_rem[1:])
+                # phrased this way to catch also the case where len(str_rem) == seq_rem[0]
+                if str_rem[seq_rem[0] : seq_rem[0] + 1] != "#":
+                    retval += combos(str_rem[seq_rem[0] + 1 :], seq_rem[1:])
                 else:
                     retval += 0
         return retval
@@ -57,6 +61,7 @@ def handle_line2(line: str):
     rval = combos(fixrecord, tuple(seqnums))
     # print("DBG", line, ":", rval)
     return rval
+
 
 data = aoc_util.get_data_lines(12)
 print(sum(handle_line1(ln) for ln in data))
