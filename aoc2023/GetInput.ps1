7a337ad742a9ed0331dd252d451bec655a460fed
Daniel Martin <fizbin@gmail.com>
2023-12-23 15:46:50 -0500

    Update GetInput.ps1 to add User-Agent

--- aoc2023/GetInput.ps1
+++ aoc2023/GetInput.ps1
@@ -11,4 +11,4 @@ $cookie.Value = (Get-Content session.txt -Raw).Trim()
 $cookie.Domain = "adventofcode.com"
 $session.Cookies.Add($cookie);
 
-Invoke-WebRequest $remoteFileLocation -WebSession $session -TimeoutSec 900 -OutFile $downloadToPath
+Invoke-WebRequest $remoteFileLocation -UserAgent "https://github.com/fizbin/adventofcode/blob/main/aoc2023/GetInput.ps1 by fizbin@gmail.com" -WebSession $session -TimeoutSec 900 -OutFile $downloadToPath
