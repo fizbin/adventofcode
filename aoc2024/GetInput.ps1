d61fcbde24b1f59d7d61f6424cbfa9db446711c0
Daniel Martin <fizbin@gmail.com>
2024-12-01 10:07:16 -0500

    Advent of code 2024 setup

---
+++ aoc2024/GetInput.ps1
@@ -0,0 +1,16 @@
+param([Parameter(Mandatory)] [int]$daynum)
+$downloadToPath = "aoc$daynum.in"
+$remoteFileLocation = "https://adventofcode.com/2024/day/$daynum/input"
+
+$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+
+$cookie = New-Object System.Net.Cookie 
+
+$cookie.Name = "session"
+$cookie.Value = (Get-Content session.txt -Raw).Trim()
+$cookie.Domain = "adventofcode.com"
+$session.Cookies.Add($cookie);
+
+# Needed because it's invalid (RFC9110) to include a URL in a User-Agent string
+$PSDefaultParameterValues['Invoke-WebRequest:SkipHeaderValidation'] = $true
+Invoke-WebRequest $remoteFileLocation -UserAgent "https://github.com/fizbin/adventofcode/blob/main/aoc2024/GetInput.ps1 by fizbin-at-gmail.com" -WebSession $session -TimeoutSec 900 -OutFile $downloadToPath
---
+++ aoc2024/golang/go.mod
@@ -0,0 +1,4 @@
+module github.com/fizbin/adventofcode/aoc2024/golang
+
+go 1.23
+
