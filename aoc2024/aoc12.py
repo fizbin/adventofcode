e95e65e235651b351682abdc78952eaf12649002
Daniel Martin <fizbin@gmail.com>
2024-12-12 08:58:55 -0500

    Cleanup 2024 day 12 python

--- aoc2024/aoc12.py
+++ aoc2024/aoc12.py
@@ -1,6 +1,7 @@
 from aoc_util import get_data_lines
 import numpy as np
 import scipy.ndimage
+import itertools
 
 
 # convenience to change defaults
@@ -10,22 +11,48 @@ def convolve(input, weights, output=None, mode="constant", cval=0, origin=0):
 
 npgrid = np.genfromtxt(get_data_lines(12), dtype=str, delimiter=1)
 
-asnumgrid = np.array(list(range(npgrid.shape[0] * npgrid.shape[1]))).reshape(npgrid.shape) + 1
-
-asnumgrid = np.zeros(shape=npgrid.shape, dtype=int)
-ltr = npgrid[0][0]
-n = 1
-for x in range(npgrid.shape[0]):
-    n = 1 + x * npgrid.shape[1]
-    for y in range(npgrid.shape[1]):
-        if ltr != npgrid[x][y]:
-            n += 1
-            ltr = npgrid[x][y]
-        if x > 0 and npgrid[x - 1][y] == npgrid[x][y] and asnumgrid[x - 1][y] != n:
-            val1 = asnumgrid[x - 1][y]
-            val2 = n
-            asnumgrid = np.where(asnumgrid == val1, val2, asnumgrid)
-        asnumgrid[x][y] = n
+asnumgrid = np.fromiter(itertools.count(1), count=npgrid.shape[0] * npgrid.shape[1], dtype=int).reshape(npgrid.shape)
+done = False
+while not done:
+    done = True
+    for x in range(npgrid.shape[0]-1, -1, -1):
+        for y in range(npgrid.shape[1]-1, -1, -1):
+            if (
+                0 <= x - 1 < npgrid.shape[0]
+                and asnumgrid[x - 1, y] != asnumgrid[x, y]
+                and npgrid[x - 1, y] == npgrid[x, y]
+            ):
+                nval = max(asnumgrid[x - 1, y], asnumgrid[x, y])
+                done = False
+                asnumgrid[x - 1, y] = nval
+                asnumgrid[x, y] = nval
+            if (
+                0 <= x + 1 < npgrid.shape[0]
+                and asnumgrid[x + 1, y] != asnumgrid[x, y]
+                and npgrid[x + 1, y] == npgrid[x, y]
+            ):
+                nval = max(asnumgrid[x + 1, y], asnumgrid[x, y])
+                done = False
+                asnumgrid[x + 1, y] = nval
+                asnumgrid[x, y] = nval
+            if (
+                0 <= y - 1 < npgrid.shape[1]
+                and asnumgrid[x, y - 1] != asnumgrid[x, y]
+                and npgrid[x, y - 1] == npgrid[x, y]
+            ):
+                nval = max(asnumgrid[x, y - 1], asnumgrid[x, y])
+                done = False
+                asnumgrid[x, y - 1] = nval
+                asnumgrid[x, y] = nval
+            if (
+                0 <= y + 1 < npgrid.shape[1]
+                and asnumgrid[x, y + 1] != asnumgrid[x, y]
+                and npgrid[x, y + 1] == npgrid[x, y]
+            ):
+                nval = max(asnumgrid[x, y + 1], asnumgrid[x, y])
+                done = False
+                asnumgrid[x, y + 1] = nval
+                asnumgrid[x, y] = nval
 
 total = 0
 for rgnum in set(list(asnumgrid.flatten())):
@@ -42,24 +69,7 @@ for rgnum in set(list(asnumgrid.flatten())):
     fencesz += (fence == 1).astype(int).sum()
     total += area * fencesz
 
-print(total)
-
-
-def count_regions(mygrid):
-    outgrid = np.zeros_like(mygrid, dtype=int)
-    for x in range(mygrid.shape[0]):
-        n = 1 + x * mygrid.shape[1]
-        for y in range(mygrid.shape[1]):
-            if not mygrid[x][y]:
-                n += 1
-                continue
-            if x > 0 and mygrid[x - 1][y] and outgrid[x - 1][y] != n:
-                val1 = outgrid[x - 1][y]
-                val2 = n
-                outgrid = np.where(outgrid == val1, val2, outgrid)
-            outgrid[x][y] = n
-    return len(set(x for x in outgrid.flatten() if x != 0))
-
+print("Part 1:", total)
 
 total = 0
 trgnum = asnumgrid[0, 0]
@@ -75,4 +85,4 @@ for rgnum in sorted(set(list(asnumgrid.flatten()))):
     nedges += ((edge_detect2 & (4 | (4 * 16))) == 4).astype(int).sum()
     total += area * nedges
 
-print(total)
+print("Part 2:", total)
