034a3cbbeedd29da729d9a43097dd2069a05b9f7
Daniel Martin <fizbin@gmail.com>
2024-12-18 09:59:19 -0500

    Cleanup of haskell for 2024 day 18

--- aoc2024/aoc18.hs
+++ aoc2024/aoc18.hs
@@ -1,10 +1,7 @@
 import Data.Char (isDigit)
-import Data.List
 import Data.Map (Map)
 import Data.Map.Strict qualified as M
 import Data.Maybe (fromJust)
-import Data.PQueue.Min qualified as PM
-import Data.Set qualified as S
 import Dijkstra (dijkstra)
 import System.Environment (getArgs)
 
@@ -16,8 +13,11 @@ findints ('-' : x : xs) | isDigit x = case findints (x : xs) of
 findints (x : xs) | not (isDigit x) = findints xs
 findints s = let (n, s') = head (reads s) in n : findints s'
 
+blankMap :: Map (Int, Int) Char
+blankMap = M.fromList [((x, y), '.') | x <- [0 .. 70], y <- [0 .. 70]]
+
 fillMap :: [(Int, Int)] -> Map (Int, Int) Char
-fillMap fallen = M.fromList ((,'#') <$> fallen) `M.union` M.fromList [((x, y), '.') | x <- [0 .. 70], y <- [0 .. 70]]
+fillMap fallen = M.fromList ((,'#') <$> fallen) `M.union` blankMap
 
 getNeighbors :: Map (Int, Int) Char -> (Int, Int) -> [(Int, Int)]
 getNeighbors mp (x, y) =
@@ -25,15 +25,14 @@ getNeighbors mp (x, y) =
    in filter (\n -> Just '.' == M.lookup n mp) nbs
 
 findThreshold :: [(Int, Int)] -> (Int, Int)
-findThreshold lst = go 1 (length lst)
+findThreshold lst = go 0 (length lst)
   where
     go a b | b - a <= 1 = lst !! a
     go a b =
       let m = (a + b) `div` 2
           one = 1 :: Int
-          zero = 0 :: Int
           mymap = fillMap (take m lst)
-       in case dijkstra (((,one) <$>) <$> getNeighbors mymap) (zero, zero) (== (70, 70)) of
+       in case dijkstra (((,one) <$>) <$> getNeighbors mymap) (0, 0) (== (70, 70)) of
             Nothing -> go a m
             _ -> go m b
 
@@ -45,9 +44,8 @@ main = do
   let parseLine ln = let xs = findints ln in (head xs, head (tail xs))
   let mydata = parseLine <$> mydatastrs
   let mymap = fillMap (take 1024 mydata)
-  let zero = 0 :: Int
   let one = 1 :: Int
   putStr "Part 1: "
-  print $ snd $ fromJust $ dijkstra (((,one) <$>) <$> getNeighbors mymap) (zero, zero) (== (70, 70))
+  print $ snd $ fromJust $ dijkstra (((,one) <$>) <$> getNeighbors mymap) (0, 0) (== (70, 70))
   putStr "Part 2: "
   putStrLn $ filter ('(' /=) $ filter (')' /=) $ show $ findThreshold mydata
