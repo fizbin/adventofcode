1dfcac9a60c99def6dd84ffaed6096cf4e678734
Daniel Martin <fizbin@gmail.com>
2024-12-19 10:16:57 -0500

    Cleanup on haskell for 2024 day 19

--- aoc2024/aoc19.hs
+++ aoc2024/aoc19.hs
@@ -9,14 +9,12 @@ import Data.Set qualified as S
 import System.Environment (getArgs)
 
 findCombos :: S.Set String -> String -> Int
-findCombos towels target = caf !! targetl
+findCombos towels target = last caf
   where
     -- (caf !! k) is the number of ways to make (take k target)
     caf = map findAns $ inits target
-    targetl = length target
     findAns "" = 1
-    findAns tgt =
-      sum [caf !! clen | (t, clen) <- zip (tails tgt) [0 ..], t `S.member` towels]
+    findAns tgt = sum [cafval | (t, cafval) <- zip (tails tgt) caf, t `S.member` towels]
 
 main :: IO ()
 main = do
