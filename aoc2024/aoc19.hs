b786d6ec5b0a61800197ecfe0a999d69b4c830c4
Daniel Martin <fizbin@gmail.com>
2024-12-19 01:34:00 -0500

    Cleanup/formattinf of 2024 day 19 haskell

--- aoc2024/aoc19.hs
+++ aoc2024/aoc19.hs
@@ -1,36 +1,30 @@
 {-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Redundant <$>" #-}
-import System.Environment(getArgs)
+
+import Data.List (isPrefixOf)
 import Data.List.Split (splitOn)
-import qualified Data.Map as M
-import Data.List
-import Control.Monad.State.Strict
+import System.Environment (getArgs)
 
 findCombos :: [String] -> String -> Int
-findCombos towels target = evalState (doCalc target) M.empty
+findCombos towels target = caf !! targetl
   where
-    doCalc :: String -> State (M.Map String Int) Int
-    doCalc "" = pure 1
-    doCalc tgt = do
-      prev <- gets (M.lookup tgt)
-      case prev of
-        Just ans -> pure ans
-        Nothing -> do
-          subAns <- forM (filter (`isPrefixOf` tgt) towels) $
-            \towel -> doCalc (drop (length towel) tgt)
-          let ans = sum subAns
-          modify (M.insert tgt ans)
-          pure ans
-
+    caf = 1 : map findAns [1 ..]
+    targetl = length target
+    findAns n =
+      let tgtTail = drop (targetl - n) target
+          poss = [caf !! (n - length t) | t <- towels, t `isPrefixOf` tgtTail]
+       in sum poss
 
 main :: IO ()
 main = do
   args <- getArgs
   let filename = if null args then "aoc19.in" else head args
-  (towelspec, patspec) <- splitOn "\n\n" <$> readFile filename >>= \case
-     [a, b] -> pure (a, b)
-     _ -> ioError (userError "Bad file structure; expected two paragraphs")
+  (towelspec, patspec) <-
+    splitOn "\n\n" <$> readFile filename >>= \case
+      [a, b] -> pure (a, b)
+      _ -> ioError (userError "Bad file structure; expected two paragraphs")
   let towels = splitOn ", " towelspec
   let combos = findCombos towels <$> lines patspec
   print $ length $ filter (/= 0) combos
