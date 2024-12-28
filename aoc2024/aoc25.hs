856f98109a802baa8d9f357c6e423f5e2c28ebbf
Daniel Martin <fizbin@gmail.com>
2024-12-28 15:19:53 -0500

    2024 day 25 in haskell

--- aoc2024/aoc25.hs
+++ aoc2024/aoc25.hs
@@ -1,8 +1,31 @@
-import System.Environment(getArgs)
+import Data.Either (lefts, rights)
+import Data.List.Split (splitOn)
+import System.Environment (getArgs)
+
+newtype Lock = Lock [Int] deriving (Show)
+
+newtype Key = Key [Int] deriving (Show)
+
+countPounds :: [String] -> [Int]
+countPounds =
+  foldr
+    (\v -> zipWith (+) [if ch == '#' then 1 else 0 | ch <- v])
+    (repeat 0)
+
+parsePara :: [String] -> Either Lock Key
+parsePara [] = error "empty para"
+parsePara para | all (== '#') (head para) = Left $ Lock $ countPounds (tail para)
+parsePara para | all (== '#') (last para) = Right $ Key $ countPounds (init para)
+parsePara para = error $ "Bad para: " ++ show para
+
+fits :: Lock -> Key -> Bool
+fits (Lock ls) (Key ks) = maximum (zipWith (+) ls ks) < 6
 
 main :: IO ()
 main = do
   args <- getArgs
   let filename = if null args then "aoc25.in" else head args
-  mydata <- lines <$> readFile filename
-  print $ length mydata
+  pieces <- splitOn "\n\n" <$> readFile filename
+  let parsed = parsePara . lines <$> pieces
+  putStr "Part 1: "
+  print $ length [() | l <- lefts parsed, k <- rights parsed, fits l k]
