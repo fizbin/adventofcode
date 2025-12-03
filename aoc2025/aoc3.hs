208a01a64689661eab140ad113ba004449fee76e
Daniel Martin <fizbin@gmail.com>
2025-12-03 02:39:59 -0500

    Day 3: improved haskell solution

--- aoc2025/aoc3.hs
+++ aoc2025/aoc3.hs
@@ -1,43 +1,28 @@
-import Control.Monad (guard)
 import Data.List (tails)
-import Data.Map.Lazy hiding (take)
 import System.Environment (getArgs)
 
 part1 :: String -> Int
 part1 [] = 0
-part1 line = maximum $ fmap go (tails line)
+part1 line = maximum $ map go (tails line)
   where
-    rd :: Char -> Int
-    rd = read . (: [])
     go :: String -> Int
     go [] = -1
     go [_] = -1
-    go (x:xs) = read [x] * 10 + maximum (fmap (fromIntegral . rd) xs)
+    go (x:xs) = read [x, maximum xs]
 
 part2 :: String -> Integer
 part2 [] = 0
-part2 line0 = ansMap ! (12, line0)
+part2 line0 = read $ go 12 line0
   where
-    pow10 :: Int -> Integer
-    pow10 x = 10 ^ x
-    go :: (Int, String) -> Integer
-    go (0, _) = -1
-    go (_, "") = -1
-    go (1, line) = maximum $ fmap (read . (: [])) line
-    go (i, line) =
-      maximum $ do
-        let maxStart = go (1, take (length line - i + 1) line)
-        let powBoost = pow10 (i - 1)
-        (firstChar:rest) <- take (length line - i + 1) $ tails line
-        let firstVal = read [firstChar]
-        guard (firstVal == maxStart)
-        return $ powBoost * firstVal + (ansMap ! (i - 1, rest))
-    ansMap :: Map (Int, String) Integer
-    ansMap =
-      fromList $ do
-        ival <- [0 .. 12]
-        line <- tails line0
-        return ((ival, line), go (ival, line))
+    go :: Int -> String -> String
+    go 0 _ = "shouldn't get here"
+    go _ "" = error "shouldn't get here either"
+    go 1 line = [maximum line]
+    go i line =
+      let maxChar = maximum $ take (length line - i + 1) line
+      in case dropWhile (/= maxChar) line of
+        (firstChar:rest) -> firstChar : go (i-1) rest
+        [] -> error "also shouldn't get here"
 
 main :: IO ()
 main = do
@@ -47,5 +32,5 @@ main = do
           [] -> "aoc3.in"
           (x:_) -> x
   batteries <- lines <$> readFile filename
-  print $ sum $ fmap part1 batteries
-  print $ sum $ fmap part2 batteries
+  print $ sum $ map part1 batteries
+  print $ sum $ map part2 batteries
